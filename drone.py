ff6431c793015d36c534abcf08c3a633eee85a0e
Nono <105313036+nonosyrn@users.noreply.github.com>
2022-05-12 11:38:06 -0700

    Update drone.py

--- drone.py
+++ drone.py
@@ -1,7 +1,37 @@
 from djitellopy import tello
 from time import sleep
 
-me = tello.Tello()
-me.connect()
+tello = tello.Tello()
 
 print(me.get_battery())
+
+#Starting up
+tello.connect()
+tello.takeoff()
+tello.move_up(120)
+sleep(0.2)
+
+#Moving Forward
+tello.move_forward(400)
+tello.rotate_counter_clockwise(90)
+
+#Rotating across the Room
+tello.move_forward(400)
+sleep(0.1)
+tello.rotate_clockwise(90)
+sleep(0.1)
+tello.move_forward(400)
+sleep(1)
+
+#Moving Back to the other Side
+tello.flip_back()
+sleep(0.1)
+tello.move_back(500)
+tello.move_back(300)
+
+#Landing
+tello.rotate_clockwise(90)
+sleep(0.1)
+tello.moveforward(400)
+sleep(1)
+tello.land()
